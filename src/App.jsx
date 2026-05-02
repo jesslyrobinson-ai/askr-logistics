88e9bb248f741e90b1299ae6143ae2b5063f3b11
Jessly Robinson <jesslyrobinson@gmail.com>
2026-05-01 23:43:14 -0400

    show dashboard instead of landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 
 function App() {
   return <Dashboard />
-}
-
-export default App
-import Dashboard from "./pages/Dashboard"
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  )
 }
 
 export default App
